10c84c63faf6a9d257161228f88f9871ec9670ed
Tim Lavreniev <tim98lav@gmail.com>
2016-10-13 16:32:29 +0300

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,86 @@
+// calculator.cpp: определяет точку входа для консольного приложения.
+//
+
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+double sum(float a, unsigned int b);
+double sub(float a, unsigned int b);
+double div(float a, unsigned int b);
+double mult(float a, unsigned int b);
+double square(float a, unsigned int b);
+double pow(float a, unsigned int b);
+
+double sum(float a, unsigned int b)
+{
+	return a + b;
+}
+
+double sub(float a, unsigned int b)
+{
+	return a - b;
+}
+
+double div(float a, unsigned int b)
+{
+	return a / b;
+}
+
+double mult(float a, unsigned int b)
+{
+	return a * b;
+}
+
+double square(float a)
+{
+	double c = 0, res;
+	res = a;
+	while (c - res > 0.00001 || c - res < -0.00001)
+	{
+		c = res;
+		res = (c + a / res) / 2;
+	}
+	return res;
+}
+
+double squar(float a)
+{
+	double c = 0, res;
+	res = a;
+	while (c - res > 0.00001 || c - res < -0.00001)
+	{
+		c = res;
+		res = (c + a / res) / 2;
+	}
+	return res;
+}
+
+double pow(float a, unsigned int b)
+{
+	double res = 1;
+	for (; b > 0; b--)
+	{
+		res = res * a;
+	}
+	return res;
+}
+
+int main()
+{
+	float a = 10.000;
+	unsigned int b = 4;
+	cout << "a + b = " << sum(a, b) << endl;
+	cout << "a - b = " << sub(a, b) << endl;
+	cout << "a / b = " << div(a, b) << endl;
+	cout << "a * b = " << mult(a, b) << endl;
+	cout << "a^1/2 = " << squar(a) << endl;
+	cout << "b^1/2 = " << squar(b) << endl;
+	cout << "a^b = " << pow(a, b) << endl;
+	_getch();
+	return 0;
+}
