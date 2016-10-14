e94708cb02f5230000ea6eb4a8ca63a51a2db25f
Tim Lavreniev <tim98lav@gmail.com>
2016-10-14 11:20:37 +0300

    Update calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -1,4 +1,4 @@
-// calculator.cpp: îïğåäåëÿåò òî÷êó âõîäà äëÿ êîíñîëüíîãî ïğèëîæåíèÿ.
+// calculator.cpp: Ã®Ã¯Ã°Ã¥Ã¤Ã¥Ã«Ã¿Ã¥Ã² Ã²Ã®Ã·ÃªÃ³ Ã¢ÃµÃ®Ã¤Ã  Ã¤Ã«Ã¿ ÃªÃ®Ã­Ã±Ã®Ã«Ã¼Ã­Ã®Ã£Ã® Ã¯Ã°Ã¨Ã«Ã®Ã¦Ã¥Ã­Ã¨Ã¿.
 //
 
 
@@ -48,14 +48,14 @@ double square(float a)
 	return res;
 }
 
-double squar(float a)
+double squar(float b)
 {
 	double c = 0, res;
-	res = a;
+	res = b;
 	while (c - res > 0.00001 || c - res < -0.00001)
 	{
 		c = res;
-		res = (c + a / res) / 2;
+		res = (c + b / res) / 2;
 	}
 	return res;
 }
