eb13a12f887c96b231c9eab3c59708fb097b401e
Tim Lavreniev <tim98lav@gmail.com>
2016-11-22 10:31:52 +0300

    Create calculator.hpp

---
+++ include/calculator.hpp
@@ -0,0 +1,6 @@
+double sum(float a, unsigned int b);
+double sub(float a, unsigned int b);
+double div(float a, unsigned int b);
+double mult(float a, unsigned int b);
+double square(float a, unsigned int b);
+double pow(float a, unsigned int b);
