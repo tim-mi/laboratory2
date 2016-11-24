7507ed6289b157b1befe93df3a6dc450f52e28fd
Tim Lavreniev <tim98lav@gmail.com>
2016-11-24 15:56:42 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,39 +2,39 @@
 #include <catch.hpp>
 
 SCENARIO("calculator sum", "[sum]") {
-    double a = 5;
-    double b = 9;
+    float a = 5;
+    unsigned int b = 9;
 
     double rt = sum( a, b);
       
       REQUIRE( rt == 14);
 }
 SCENARIO("calculator sub", "[sub]") {
-    double a = 10;
-    double b = 1;
+    float a = 10;
+    unsigned int b = 1;
     
     double rt = sub( a, b);
     
       REQUIRE( rt == 9);
 }
 SCENARIO("calculator div", "[div]"){
-    double a = 16;
-    double b = 4;
+    float a = 16;
+    unsigned int b = 4;
     
     double rt = div( a, b);
     
       REQUIRE( rt == 4);
 }
 SCENARIO("calculator mult","[mult]"){
-    double a = 6;
-    double b = 7;
+    float a = 6;
+    unsigned int b = 7;
     
     double rt = mult( a, b);
     
       REQUIRE( rt == 42);
 }
 SCENARIO("calculator pow","[pow]"){
-    double a = 2;
+    float a = 2;
     unsigned int b = 10;
     
     double rt = pow( a, b);
@@ -42,7 +42,7 @@ SCENARIO("calculator pow","[pow]"){
       REQUIRE( rt == 1024);
 }
 SCENARIO("calculator square","[square]"){
-    double a = 36;
+    float a = 36;
     
     double rt = square( a);
     
