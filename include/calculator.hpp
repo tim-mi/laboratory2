bc0410342301e517e883af2091a2b391f990e8d2
Tim Lavreniev <tim98lav@gmail.com>
2016-11-24 16:20:36 +0300

    Update calculator.hpp

--- include/calculator.hpp
+++ include/calculator.hpp
@@ -2,5 +2,6 @@ double sum(float a, unsigned int b);
 double sub(float a, unsigned int b);
 double div(float a, unsigned int b);
 double mult(float a, unsigned int b);
-double square(float a, unsigned int b);
+double square(float a);
+double square(unsigned int b);
 double pow(float a, unsigned int b);
