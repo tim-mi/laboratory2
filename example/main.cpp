04575b9f2f5207dda2fd1f3ce9e6eaf2711eb72f
Tim Lavreniev <tim98lav@gmail.com>
2016-11-25 15:10:28 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -5,18 +5,35 @@ using namespace std;
 
 int main()
 {
-        float a;
-	unsigned int b;
+	float a;
+	int b;
 	cout << "a=";
 	cin >> a;
 	cout << "b=";
 	cin >> b;
 	cout << "a + b = " << sum(a, b) << endl;
+
 	cout << "a - b = " << sub(a, b) << endl;
-	cout << "a / b = " << div(a, b) << endl;
+
+	double *res = div(a, b);
+	if (res)
+		cout << "a / b = " << *div(a, b) << endl;
+	else
+		cout << "a/0 -> error" << endl;
+	delete res;
+
 	cout << "a * b = " << mult(a, b) << endl;
-	cout << "a^1/2 = " << square(a) << endl;
-	cout << "b^1/2 = " << squar(b) << endl;
+
+	double *r = square(a);
+	if (r) cout << "a^1/2=" << *square(a) << endl;
+	else cout << "a < 0 -> error" << endl;
+	delete r;
+
+	double *d = squar(b);
+	if (d) cout << "b^1/2=" << *squar(b) << endl;
+	else cout << "b < 0 -> error" << endl;
+	delete d;
+
 	cout << "a^b = " << pow(a, b) << endl;
 	system("pause");
 	return 0;
